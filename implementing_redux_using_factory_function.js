a311454ba60de802d40dc8e3824eac15f6c33004
riyad <hifazakhatun1108@gmail.com>
2021-06-07 12:55:23 +0600

    implement redux using factory function added

---
+++ implementing_redux_using_factory_function.js
@@ -0,0 +1,46 @@
+// Implementing Redux Prototype Using Factory Function in Js
+
+function store(initialState, reducer){
+
+
+
+   const store = {};
+	
+	store.state = initialState;
+	store.subscribers = [];
+
+	store.subscribe = (listener) => {
+	
+	if(typeof listener === 'function') store.subscribers.push(listener);
+
+	}
+
+	store.dispatch = (action) => {
+	
+	store.state = reducer(store.state, action);
+	store.subscriber.forEach(sub => sub());
+	
+	}
+
+}
+
+
+const reducer = (state, action) {
+
+	switch(action.type) {
+
+	case 'add': return state + 1;
+	case 'sub': return state - 1;
+	default: return state;
+
+	}
+}
+
+
+
+	const store = store(reducer);
+
+	store.subscribe(() => console.log(getSate());
+
+	store.dispatch({type: 'add'});
+
